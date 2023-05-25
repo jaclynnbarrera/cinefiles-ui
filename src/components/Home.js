9a0dbc62f3474651058120433fd00227e3dc8950
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-05-25 14:27:17 -0400

    adds very basic skeleton of home layout, nav and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,24 @@
+import cinefilesLogo from "../images/Cine_Logo_1.png";
+import "./home.css";
+
 function Home() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <div className="home-container">
+      <div className="nav">
+        <div className="search-bar">
+          <p>search bar here</p>
+        </div>
+        <img className="cf-logo" src={cinefilesLogo} alt="Cinefiles logo" />
+        <div className="time">
+          <p>time here</p>
+        </div>
+      </div>
+      <div>
+        <div>side bar</div>
+        <div>main content</div>
+      </div>
+      <div>footer</div>
+    </div>
   );
 }
 
