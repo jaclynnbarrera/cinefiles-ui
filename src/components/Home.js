0faba060a3c8b111f509299adb2c8ea595a353cd
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-05-25 14:38:16 -0400

    adds logic to display current time

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import cinefilesLogo from "../images/Cine_Logo_1.png";
 import "./home.css";
 
 function Home() {
+  const [time, setTime] = useState("00:00");
+
+  useEffect(() => {
+    setInterval(updateTime(), 6000);
+  });
+
+  const updateTime = () => {
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    setTime(time);
+  };
+
   return (
     <div className="home-container">
       <div className="nav">
@@ -10,7 +23,7 @@ function Home() {
         </div>
         <img className="cf-logo" src={cinefilesLogo} alt="Cinefiles logo" />
         <div className="time">
-          <p>time here</p>
+          <p>{time}</p>
         </div>
       </div>
       <div>
